c080ff459bdbaaf91b19707be5828a3979565824
Zane <zanewnch@gmail.com>
2024-02-25 16:54:15 +0800

    temp.

---
+++ linkedList/Main.java
@@ -0,0 +1,33 @@
+package algorithm.linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Node {
+    String value;
+    Node next;
+
+    public Node(String value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    public LinkedList(){
+        this.head = null;
+    }
+
+    // add element
+    public void push(){
+
+    }
+    // delete ele
+    public void pop(){
+
+    }
+}
