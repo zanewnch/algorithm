4a42b2236ab13a3f10a6c2d54bebd7a677f9f34b
zanewnch <zanewnch@gmail.com>
2024-02-28 12:13:10 +0800

    temp.

--- binary-tree/lc-94.js
+++ binary-tree/lc-94.js
@@ -7,6 +7,7 @@ class TreeNode {
 }
 
 var inorderTraversal = function (root) {
+  // left-root-right
   let result = [];
   helper(root, result);
   return result;
@@ -25,5 +26,38 @@ function helper(node, result) {
   }
 }
 
+function arrayToTreeNode(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  let i = 1;
+
+  while (i < arr.length) {
+    const node = queue.shift();
+
+    if (arr[i] !== null) {
+      node.left = new TreeNode(arr[i]);
+      queue.push(node.left);
+    }
+
+    i++;
+
+    if (i < arr.length && arr[i] !== null) {
+      node.right = new TreeNode(arr[i]);
+      queue.push(node.right);
+    }
+
+    i++;
+  }
+
+  return root;
+}
+
+
+
 let root = [1,null,2,3];
-console.log(inorderTraversal(root));
+const rootNode = arrayToTreeNode(root);
+console.log(inorderTraversal(rootNode));
